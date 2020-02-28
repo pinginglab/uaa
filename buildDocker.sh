a9d7d756f3edc412c5b74da0f08dd42e32407a8e
yerikyu <treestore@example.com>
2020-02-28 23:01:12 +0800

    更新打包脚本

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./gradlew bootJar -Pprod jibDockerBuild
 #docker tag uaa:latest registry.cn-hangzhou.aliyuncs.com/pingsec/compose/uaa:latest
-docker tag uaa:latest ccr.ccs.tencentyun.com/pingsec/compose/uaa:1.0
-docker push ccr.ccs.tencentyun.com/pingsec/compose/uaa:1.0
+docker tag uaa:latest ccr.ccs.tencentyun.com/compose/uaa:1.0
+docker push ccr.ccs.tencentyun.com/compose/uaa:1.0
